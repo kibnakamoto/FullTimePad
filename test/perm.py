db0ff7b973172e22ba8859200e92b5dc6989d163
Kibnakamoto <kibnakanoto@protonmail.com>
2025-02-10 00:26:17 -0500

    added documentation about why it\'s secure

--- test/perm.py
+++ test/perm.py
@@ -1,7 +1,7 @@
 a = [i for i in range(32)]
-b = a[:]
+b = a[:] 
 p = a[:]
-V = []
+V = [] # resulting matrix
 
 # derivation of the n_V matrix
 for k in range(4):
@@ -11,27 +11,28 @@ for k in range(4):
         p[i+16] = b[i*4+2]
         p[i+24] = b[i*4+3]
     b = p[:]
-    #print(p)
+    print(p)
     V.append(p[:])
     copy = p[:]
     for m in range(3):
         for i in range(32//4):
             for n in range(4):
                 p[i*4+n]  = copy[(1+n+m)%4+i*4]
-        #print(p)
+        print(p)
         V.append(p[:])
     b = p[:]
 
-for i in range(16):
-    for j in range(32):
-        if V[i][j] < 9:
-            phantom = "\phantom{0}"
-        else:
-            phantom = ""
-            
-        if j < 31:
-
-            print(phantom + str(V[i][j]), end=' & ')
-        else: 
-            print(phantom + str(V[i][j]), end=' \\\\')
-    print() 
+# print the matrix to represent in latex document
+#for i in range(16):
+#    for j in range(32):
+#        if V[i][j] < 9:
+#            phantom = "\phantom{0}"
+#        else:
+#            phantom = ""
+#            
+#        if j < 31:
+#
+#            print(phantom + str(V[i][j]), end=' & ')
+#        else: 
+#            print(phantom + str(V[i][j]), end=' \\\\')
+#    print() 
