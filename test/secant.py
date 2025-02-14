a7bb9f60b08404689cc15e2c6e72f89929fb7409
Kibnakamoto <kibnakanoto@protonmail.com>
2025-02-14 12:38:37 -0500

    finished birthday attack calculations

--- test/secant.py
+++ test/secant.py
@@ -4,8 +4,10 @@ from decimal import Decimal, getcontext
 
 getcontext().prec = 200
 
-b = Decimal(0xfffffffb**8)
-# b = Decimal(365)
+# uncomment b to find k for f(k) = 0
+# b = Decimal(0xfffffffb**8) # number of combinations with galois field
+b = Decimal(2**256) # number of combinations without galois field
+# b = Decimal(365) # for the birthday problem
 
 def log(x, base=Decimal(math.e)):
     if base==Decimal(math.e):
@@ -20,32 +22,27 @@ def f(k):
     return c - (x*log(x) + k  + log(b)*k + log(1/Decimal(math.pi)**3 + x*(1+4*x*(1+2*x)))/6)
 
 def secant(x0,x1,e,N):
-    print('\n\n*** SECANT METHOD IMPLEMENTATION ***')
     step = 1
     condition = True
     x2 = 0.0
     while condition:
         if f(x0) == f(x1): 
-            print('Divide by zero error!')
+            print('division of zero')
             break
-
         x2 = (x0 - (x1-x0)*f(x0)/( f(x1) - f(x0) ))
-        print(f'Iteration-{step}, x2 = {x2} and f(x2) = {f(x2)}')
+        print(f'Iteration {step}, x2 = {x2} and f(x2) = {f(x2)}')
         x0 = x1
         x1 = x2
         step = step + 1
-
         if step > N:
             print('Not Convergent!')
             break
-
         condition = abs(f(x2)) > e
-
-    print(f'\n Required root is: {x2}')
+    print(f'\n root: {x2}')
     return x2
 
-x0 = Decimal(2**172)
-x1 = Decimal(1496577676626844588240573268701473812127674924007424)
+x0 = Decimal(1/2+math.sqrt(Decimal(1/4) + 2*Decimal(2).ln()*b))
+x1 = Decimal(math.sqrt(b))
 e = Decimal(1e-200)
 
 N = 100000
